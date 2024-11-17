503f672005c2a5ff827b0525494ef996d5c483e1
Ben Schalom <2433366@carrefour.cegepvicto.ca>
2024-11-17 01:36:43 -0500

    Finis

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,17 @@ const erreurValidation = {
     "formatInvalide" : "Le code produit que vous avez saisie est invalide, il ne respecte pas le format requis.",
     "produitInexistant" : "Aucun produit n'est associé au code de produit saisie."
 };
-
+const zone=document.getElementById("cle-activation");
+const messageErreur=document.getElementById("message-erreur");
+const casecheck=document.getElementById('declaration');
+const textDeclaration=document.getElementById("modification");
+const couleur = getComputedStyle(document.documentElement).getPropertyValue('--couleur-texte-invalide');
 
 // Code pour provoquer une animation sur le bouton Activer
 const boutonActiver = document.querySelector('.bouton-activation');
 boutonActiver.addEventListener('click', (e) => e.target.classList.add('bouton-click'));
 boutonActiver.addEventListener('transitionend', (e) =>  e.target.classList.remove('bouton-click'));
+
 
 /**
  * Recherche dans le tableau listeProduits un produit associé a un code de produit
@@ -36,5 +41,44 @@ function isProduitExiste(codeProduit) {
 // Ajoutez votre code plus bas                                             =
 // =========================================================================
 
+ function validationCode(){
+    let test1=false;
+    let test2=false;
+    let test3=false
+    let message="";
 
+    if(REGEX_CODE_PRODUIT.test(zone.value)){
+        test1=true;
+    
+    }
+    else{
+       message+=erreurValidation["formatInvalide"]+'<br>';
+    }
+
+   if(listeProduits[zone.value]){
+       test2=true;
+    }
+    else{
+        message+=erreurValidation["produitInexistant"]+'<br>';
+    }
+
+    if(casecheck.checked){
+        test3=true;
+    }
+    else{
+        message+=erreurValidation["terme"];
+    }
+
+    if(test1==true && test2==true && test3==true)
+    {
+        messageErreur.classList.add("hidden");
+        return true;
+    }
+    else{
+        messageErreur.innerHTML=message;
+        messageErreur.classList.remove("hidden");
+        textDeclaration.style.color=couleur;
+        return false;
+    }
+}
 
